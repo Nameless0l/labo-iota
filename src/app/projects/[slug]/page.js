2f7bee92445ebf24c170d40f11ff8513988c4afa
Nameless0l <wwwmbassiloic@gmail.com>
2025-05-12 19:03:39 +0100

    version beta

--- src/app/projects/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default function ProjectPage({ params }) {
-    const { slug } = params;
-    // Récupérer les données du projet basé sur le slug
-    return (
-      <div className="container mx-auto py-16 px-4">
-        {/* Contenu du projet */}
-      </div>
-    );
-  }
